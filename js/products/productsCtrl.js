5b3088dcd3de2c52a965f94192fa29c5363194f5
marcandy <marcandy.nj@gmail.com>
2016-12-19 15:25:53 -0700

    review

--- js/products/productsCtrl.js
+++ js/products/productsCtrl.js
@@ -1,4 +1,10 @@
 angular.module('miniRoute')
-  .controller('productsCtrl', function ($scope) {
+  .controller('productsCtrl', function ($scope, $stateParams, productService) {
 
+    if(stateParams.id === 'shoes') {
+      $scope.porductData = productService.shoeData;
+    }
+    else if(stateParams.id === 'socks') {
+      $scope.productData = productService.sockData;
+    }
   })
