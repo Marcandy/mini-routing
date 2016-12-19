c3ce9616010c2fad83a8e7a55762aba4bd2642f5
marcandy <marcandy.nj@gmail.com>
2016-12-19 15:01:49 -0700

    review

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 angular.module('miniRoute', ['ui.router'])
-  
+    .config(function ($stateProvider, urlRouterProvider) {
+
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: './js/home/homeTmpl.html',
+          controller: 'homeCtrl'
+        })
+    })
